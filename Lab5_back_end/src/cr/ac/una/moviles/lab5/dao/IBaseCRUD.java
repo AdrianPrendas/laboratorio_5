fc5b789975c26e0022b7001ad29f54b04b219d43
Adrian Prendas <a6r1an@hotmail.com>
2018-05-16 23:50:47 -0600

    algunos cambios en el back_end

--- Lab5_back_end/src/cr/ac/una/moviles/lab5/dao/IBaseCRUD.java
+++ Lab5_back_end/src/cr/ac/una/moviles/lab5/dao/IBaseCRUD.java
@@ -6,9 +6,9 @@ import java.util.List;
  *
  * @author _Adrián_Prendas_
  */
-public interface IBaseCRUD<T,K> {
+public interface IBaseCRUD<K, T> {
     boolean create(T t);
-    List<T> read(T t);
+    T read(K k);
     List<T> readAll();
     boolean update(T t);
     boolean delete(K t); 
