4cccbb601cd60852d2ac41b9a9e1eefadfe8bffe
kevcalrod <kevcalrod@gmail.com>
2018-05-18 01:09:45 -0600

    Validacion

--- SistemaDeRecomendacion/app/src/main/java/com/example/kevca/sistemaderecomendacion/PagoActivity.java
+++ SistemaDeRecomendacion/app/src/main/java/com/example/kevca/sistemaderecomendacion/PagoActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,12 +15,22 @@ import com.example.kevca.sistemaderecomendacion.bl.UsuarioBL;
 import com.example.kevca.sistemaderecomendacion.domain.Carrito;
 
 public class PagoActivity extends AppCompatActivity {
-
+    private RadioButton rbtarjetas;
+    private RadioButton rbpaypall;
+    private EditText editText; //Numero
+    private EditText editText2; //Nombre tarjeta
+    private EditText editText3;
+    private EditText editText4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pago);
-
+        rbtarjetas = findViewById(R.id.radioButton_tarjets);
+        rbpaypall = findViewById(R.id.radioButton_paypal);
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editText3 = (EditText) findViewById(R.id.editText3);
+        editText4 = (EditText) findViewById(R.id.editText4);
         float total = CarritoBL.Companion.getInstance().read(
                 UsuarioBL.Companion.getSession()
         ).getPrecioTotal();
@@ -29,6 +41,8 @@ public class PagoActivity extends AppCompatActivity {
         clickButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(espaciosVerificados()){
+
                 Carrito c = CarritoBL.Companion.getInstance().read(
                         UsuarioBL.Companion.getSession()
                 );
@@ -40,7 +54,20 @@ public class PagoActivity extends AppCompatActivity {
                 c.pagar();
                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
+                }else{
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Rellene todos los espacios",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
             }
         });
+    }
+    private boolean espaciosVerificados() {
+        if ((!rbtarjetas.isChecked() && !rbpaypall.isChecked()) || editText.getText().toString().trim().equals("") || editText2.getText().toString().trim().equals("") || editText3.getText().toString().trim().equals("") || editText4.getText().toString().trim().equals("")) {
+            return false;
+        }
+        return true;
     }
 }
